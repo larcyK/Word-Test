68bc6ab0e9072ab2060d0e0bd607d0f9e2ae17b9
larcyK <40230524+larcyK@users.noreply.github.com>
2024-02-08 05:03:19 +0900

    nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,146 +1,187 @@
-import { onCleanup, onMount } from 'solid-js';
+import { Match, Switch, createSignal, onCleanup, onMount } from 'solid-js';
 import type { Component } from 'solid-js';
 import * as bootstrap from 'bootstrap';
+
+enum PageKind {
+  HOME = 'home',
+  WORD_TEST = 'word-test',
+  PROFILE = 'profile',
+  TEMPLATE = 'template',
+}
 
 const App: Component = () => {
   /**
    * This function was taken from the cheatsheet example of bootstrap.
    * You will most likely remove it if using this template.
    */
-  return (
-    <>
+  const [pageKind, setPageKind] = createSignal(PageKind.HOME);
 
-      <div ref={(tooltip) =>
-        new bootstrap.Tooltip(tooltip, {
-          selector: '[data-bs-toggle="tooltip"]',
-        })
-      }>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
-          <div class="container-fluid">
-            <a
-              class="navbar-brand"
-              href="/"
-              onClick={(e) => e.preventDefault()}
-            >
-              <img 
-                src="../public/images/icon-192x192.png"
-                width="30"
-                height="30"
-                class="d-inline-block align-text-top" 
+  return (
+  <>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+        <div class="container-fluid">
+          <a
+            class="navbar-brand"
+            href="/"
+            onClick={(e) => e.preventDefault()}
+          >
+            <img 
+              src="../public/images/icon-192x192.png"
+              width="30"
+              height="30"
+              class="d-inline-block align-text-top" 
+            />
+            Word Test
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent2"
+            aria-controls="navbarSupportedContent2"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            class="collapse navbar-collapse"
+            id="navbarSupportedContent2"
+          >
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={(e) => {
+                    setPageKind(PageKind.HOME);
+                    console.log('Home');
+                  }}
+                >
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={(e) => setPageKind(PageKind.WORD_TEST)}
+                >
+                  Word Test
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  onClick={(e) => setPageKind(PageKind.PROFILE)}
+                >
+                  Profile
+                </a>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link active"
+                  aria-current="page"
+                  
+                >
+                  Template
+                </a>
+              </li>
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  onClick={(e) => e.preventDefault()}
+                  id="navbarDropdown2"
+                  aria-role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Dropdown
+                </a>
+                <ul
+                  class="dropdown-menu"
+                  aria-labelledby="navbarDropdown2"
+                >
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Action
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Another action
+                    </a>
+                  </li>
+                  <li>
+                    <hr class="dropdown-divider" />
+                  </li>
+                  <li>
+                    <a
+                      class="dropdown-item"
+                      href="#"
+                      onClick={(e) => e.preventDefault()}
+                    >
+                      Something else here
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li class="nav-item">
+                <a
+                  class="nav-link disabled"
+                  href="#"
+                  onClick={(e) => e.preventDefault()}
+                  tabindex="-1"
+                  aria-disabled="true"
+                >
+                  Disabled
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input
+                class="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
               />
-              Word Test
-            </a>
-            <button
-              class="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent2"
-              aria-controls="navbarSupportedContent2"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div
-              class="collapse navbar-collapse"
-              id="navbarSupportedContent2"
-            >
-              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                  <a
-                    class="nav-link active"
-                    aria-current="page"
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Home
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link"
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                  >
-                    Link
-                  </a>
-                </li>
-                <li class="nav-item dropdown">
-                  <a
-                    class="nav-link dropdown-toggle"
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                    id="navbarDropdown2"
-                    aria-role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Dropdown
-                  </a>
-                  <ul
-                    class="dropdown-menu"
-                    aria-labelledby="navbarDropdown2"
-                  >
-                    <li>
-                      <a
-                        class="dropdown-item"
-                        href="#"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        class="dropdown-item"
-                        href="#"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <hr class="dropdown-divider" />
-                    </li>
-                    <li>
-                      <a
-                        class="dropdown-item"
-                        href="#"
-                        onClick={(e) => e.preventDefault()}
-                      >
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-                <li class="nav-item">
-                  <a
-                    class="nav-link disabled"
-                    href="#"
-                    onClick={(e) => e.preventDefault()}
-                    tabindex="-1"
-                    aria-disabled="true"
-                  >
-                    Disabled
-                  </a>
-                </li>
-              </ul>
-              <form class="d-flex">
-                <input
-                  class="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button class="btn btn-outline-light" type="submit">
-                  Search
-                </button>
-              </form>
-            </div>
+              <button class="btn btn-outline-light" type="submit">
+                Search
+              </button>
+            </form>
           </div>
-        </nav>
-      </div>
+        </div>
+        <div ref={(tooltip) =>
+          new bootstrap.Tooltip(tooltip, {
+            selector: '[data-bs-toggle="tooltip"]',
+          })
+        }></div>
+      </nav>
             
+      <Switch fallback={<div>404</div>}>
+        <Match when={pageKind() === PageKind.HOME}>
+          <div>Home</div>
+        </Match>
+        <Match when={pageKind() === PageKind.WORD_TEST}>
+          <div>Word Test</div>
+        </Match>
+        <Match when={pageKind() === PageKind.PROFILE}>
+          <div>Profile</div>
+        </Match>
+        <Match when={pageKind() === PageKind.TEMPLATE}>
+          <div>Template</div>
+        </Match>
+      </Switch>
     </>
   );
 };

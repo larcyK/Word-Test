533c03242608b70097c9d8d7f54bd1c94dfacd49
larcyK <40230524+larcyK@users.noreply.github.com>
2024-02-08 04:48:53 +0900

    fix nav

--- src/App.tsx
+++ src/App.tsx
@@ -9,161 +9,138 @@ const App: Component = () => {
    */
   return (
     <>
-      <div class="bd-cheatsheet container-fluid bg-body">
-        <section id="components">
-          <h2 class="sticky-xl-top fw-bold pt-3 pt-xl-5 pb-2 pb-xl-3">
-            Components
-          </h2>
 
-          <article class="my-3" id="navbar">
-            <div class="bd-heading sticky-xl-top align-self-start mt-5 mb-3 mt-xl-0 mb-xl-2">
-              <h3>Navbar</h3>
-              <a class="d-flex align-items-center" href="../components/navbar/">
-                Documentation
-              </a>
+      <div ref={(tooltip) =>
+        new bootstrap.Tooltip(tooltip, {
+          selector: '[data-bs-toggle="tooltip"]',
+        })
+      }>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+          <div class="container-fluid">
+            <a
+              class="navbar-brand"
+              href="/"
+              onClick={(e) => e.preventDefault()}
+            >
+              <img 
+                src="../public/images/icon-192x192.png"
+                width="30"
+                height="30"
+                class="d-inline-block align-text-top" 
+              />
+              Word Test
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent2"
+              aria-controls="navbarSupportedContent2"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+              class="collapse navbar-collapse"
+              id="navbarSupportedContent2"
+            >
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a
+                    class="nav-link active"
+                    aria-current="page"
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link"
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                  >
+                    Link
+                  </a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a
+                    class="nav-link dropdown-toggle"
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                    id="navbarDropdown2"
+                    aria-role="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Dropdown
+                  </a>
+                  <ul
+                    class="dropdown-menu"
+                    aria-labelledby="navbarDropdown2"
+                  >
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="#"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        Action
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="#"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        Another action
+                      </a>
+                    </li>
+                    <li>
+                      <hr class="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a
+                        class="dropdown-item"
+                        href="#"
+                        onClick={(e) => e.preventDefault()}
+                      >
+                        Something else here
+                      </a>
+                    </li>
+                  </ul>
+                </li>
+                <li class="nav-item">
+                  <a
+                    class="nav-link disabled"
+                    href="#"
+                    onClick={(e) => e.preventDefault()}
+                    tabindex="-1"
+                    aria-disabled="true"
+                  >
+                    Disabled
+                  </a>
+                </li>
+              </ul>
+              <form class="d-flex">
+                <input
+                  class="form-control me-2"
+                  type="search"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                <button class="btn btn-outline-light" type="submit">
+                  Search
+                </button>
+              </form>
             </div>
-
-            <div>
-              <div class="bd-example">
-                <nav class="navbar navbar-expand-lg navbar-dark bg-primary mt-5">
-                  <div class="container-fluid">
-                    <a
-                      class="navbar-brand"
-                      href="#"
-                      onClick={(e) => e.preventDefault()}
-                    >
-                      <img
-                        src="../assets/brand/bootstrap-logo-white.svg"
-                        width="38"
-                        height="30"
-                        class="d-inline-block align-top"
-                        alt="Bootstrap"
-                        loading="lazy"
-                      />
-                    </a>
-                    <button
-                      class="navbar-toggler"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#navbarSupportedContent2"
-                      aria-controls="navbarSupportedContent2"
-                      aria-expanded="false"
-                      aria-label="Toggle navigation"
-                    >
-                      <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div
-                      class="collapse navbar-collapse"
-                      id="navbarSupportedContent2"
-                    >
-                      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                          <a
-                            class="nav-link active"
-                            aria-current="page"
-                            href="#"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Home
-                          </a>
-                        </li>
-                        <li class="nav-item">
-                          <a
-                            class="nav-link"
-                            href="#"
-                            onClick={(e) => e.preventDefault()}
-                          >
-                            Link
-                          </a>
-                        </li>
-                        <li class="nav-item dropdown">
-                          <a
-                            class="nav-link dropdown-toggle"
-                            href="#"
-                            onClick={(e) => e.preventDefault()}
-                            id="navbarDropdown2"
-                            aria-role="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false"
-                          >
-                            Dropdown
-                          </a>
-                          <ul
-                            class="dropdown-menu"
-                            aria-labelledby="navbarDropdown2"
-                          >
-                            <li>
-                              <a
-                                class="dropdown-item"
-                                href="#"
-                                onClick={(e) => e.preventDefault()}
-                              >
-                                Action
-                              </a>
-                            </li>
-                            <li>
-                              <a
-                                class="dropdown-item"
-                                href="#"
-                                onClick={(e) => e.preventDefault()}
-                              >
-                                Another action
-                              </a>
-                            </li>
-                            <li>
-                              <hr class="dropdown-divider" />
-                            </li>
-                            <li>
-                              <a
-                                class="dropdown-item"
-                                href="#"
-                                onClick={(e) => e.preventDefault()}
-                              >
-                                Something else here
-                              </a>
-                            </li>
-                          </ul>
-                        </li>
-                        <li class="nav-item">
-                          <a
-                            class="nav-link disabled"
-                            href="#"
-                            onClick={(e) => e.preventDefault()}
-                            tabindex="-1"
-                            aria-disabled="true"
-                          >
-                            Disabled
-                          </a>
-                        </li>
-                      </ul>
-                      <form class="d-flex">
-                        <input
-                          class="form-control me-2"
-                          type="search"
-                          placeholder="Search"
-                          aria-label="Search"
-                        />
-                        <button class="btn btn-outline-light" type="submit">
-                          Search
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </nav>
-              </div>
-            </div>
-          </article>
-          
-          <div
-            ref={(tooltip) =>
-              new bootstrap.Tooltip(tooltip, {
-                selector: '[data-bs-toggle="tooltip"]',
-              })
-            }
-          >
           </div>
-        </section>
+        </nav>
       </div>
+            
     </>
   );
 };
